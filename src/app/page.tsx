b449f39302ac74984c3e9401421826cb0f3417af
SantiagoGetialAyala <brayan.getial@campusucc.edu.co>
2025-06-24 15:52:58 -0500

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+
+ import Discs from '@/components/Discs';
+ import AboutUs from '@/components/AboutUs';
+export default function Home () {
+  return (
+    <main>
+      <Discs />
+      <AboutUs />
+    </main>
+    
+  );
+}
---
+++ src/components/AboutUs.jsx
@@ -0,0 +1,10 @@
+const AboutUs = () => (
+  <section className="py-12 px-6 text-center bg-gray-100">
+    <h2 className="text-3xl font-bold mb-4">Quiénes Somos</h2>
+    <p className="max-w-2xl mx-auto text-lg text-gray-700">
+      Somos un proyecto musical que busca llevar emociones a través de cada nota. Nuestra pasión es crear y compartir música con el mundo.
+    </p>
+  </section>
+)
+
+export default AboutUs;
